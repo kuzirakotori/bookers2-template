f2e422df21d3d3d417e2da7af1c2a21cde2f50fe
EC2 Default User <ec2-user@ip-172-31-6-144.ap-northeast-1.compute.internal>
2022-06-30 02:03:48 +0000

    relationshipモデル作成まで

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   has_one_attached :profile_image
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
-  
+
   validates :name, uniqueness: true, length: { in: 2..20 }
   validates :introduction, length: { maximum: 50 }
   
